159b006207af73b12f8e2abc935a952d6ed3fa69
Gyeong-Yeon <lky4156@naver.com>
2020-11-03 11:06:34 +0900

    modify 1103 algorithm

--- Algorithm/SW Expert Academy/1959_두 개의 숫자열.py
+++ Algorithm/SW Expert Academy/1959_두 개의 숫자열.py
@@ -1,24 +1,23 @@
 T = int(input())
 for t in range(1,T+1):
+    a, b = map(int,input().split()) # a:첫번째 리스트 길이, b:두번째 리스트 길이
     A = list(map(int,input().split()))
     B = list(map(int,input().split()))
 
-    if len(A) > len(B):
-        A, B = B, A
+    if a > b: # 첫번째 리스트 길이가 더 길면, SWAP
+        A, B = B, A # 첫번째 리스트가 두번째 리스트보다 길이가 짧음
 
     sum_list = []
-    for b in range(len(B)-len(A)+1):
-        new_B = B[b:b+len(A)]
-        sum = 0
-        for i in range(len(A)):
-            for j in range(len(A)):
-                if i == j:
-                    sum += A[i] * new_B[j]
-        sum_list.append(sum)
+    for i in range(len(B)-len(A)+1): # 리스트 B의 시작점 순회하면서
+        new_B = B[i:i+a] # 리스트 A 길이만큼 새로운 리스트 B 생성
+        sum_ = 0
+        for j in range(len(A)):
+            sum_ += A[j] * new_B[j]
+        sum_list.append(sum_)
 
     result = max(sum_list)
 
-    print(f'{t} {result}')
+    print(f'#{t} {result}')
 
 
 
